9be8dfaaebda3bf02d432cb253a6814472ae1abd
DC Thomson Data Team <datateam@dctmedia.co.uk>
2025-05-30 11:14:39 +0100

    Update reporter-script.R
    April  data + financial year changes

--- script/reporter-script.R
+++ script/reporter-script.R
@@ -5,16 +5,16 @@ library(lubridate)
 
 # Add link directly from CI datastore
 
-update <- read_csv("data/datastore/MDSF_data_31 March 2025.csv", locale = locale(encoding = "Windows-1252"))
+update <- read_csv("data/datastore/MDSF_data_30 April 2025.csv", locale = locale(encoding = "Windows-1252"))
 update$ServiceName <- gsub('[^\x20-\x7E]',  '', update$ServiceName) 
 columns_keep <- c("CareService", "Subtype", "ServiceType", "ServiceName",
                   "Service_town", "Service_Postcode", "ServiceProvider", 
                 "Date_Reg", "Council_Area_Name","GradeSpread", "MinGrade_change", "Publication_of_Latest_Grading",
                   "KQ_Support_Wellbeing", "KQ_Care_and_Support_Planning", "KQ_Setting",
                   "KQ_Staff_Team", "KQ_Leadership", "KQ_Care_Play_and_Learning",
-                  "Complaints_upheld_2223", "Complaints_upheld_2324", "Complaints_upheld_2425",
-                  "Enforcements_issued_2223", "Enforcements_issued_2324", "Enforcements_issued_2425",
-                  "any_requirements_2223", "any_requirements_2324", "any_requirements_2425",
+                  "Complaints_upheld_2324", "Complaints_upheld_2425", "Complaints_upheld_2526",
+                  "Enforcements_issued_2324", "Enforcements_issued_2425", "Enforcements_issued_2526",
+                  "any_requirements_2324", "any_requirements_2425", "any_requirements_2526"
                   "Last_inspection_Date", "ServiceStatus")
 
 update <- update %>% 
@@ -49,16 +49,16 @@ care_homes <- df %>%
   select(-c(CareService, GradeSpread, ServiceStatus, KQ_Care_Play_and_Learning)) %>% 
   rowwise() %>%
   mutate(`Average` = mean(c_across(starts_with("KQ")), na.rm = TRUE)) %>% 
-  mutate(`Complaints_upheld_since_22_23` = sum(c_across(starts_with("Complaints_upheld_")), na.rm = TRUE)) %>% 
-  mutate(`Enforcements_upheld_since_22_23` = sum(c_across(starts_with("Enforcements_issued_")), na.rm = TRUE))
+  mutate(`Complaints_upheld_since_23_24` = sum(c_across(starts_with("Complaints_upheld_")), na.rm = TRUE)) %>% 
+  mutate(`Enforcements_upheld_since_23_24` = sum(c_across(starts_with("Enforcements_issued_")), na.rm = TRUE))
   
 
 
 dundee_care_homes <- care_homes %>% 
   filter(Service_town == "Dundee") %>% 
   filter(Council_Area_Name == "Dundee City") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(dundee_care_homes, "data/report/dundee_care_homes.csv")
@@ -66,8 +66,8 @@ write.csv(dundee_care_homes, "data/report/dundee_care_homes.csv")
 aberdeen_care_homes <- care_homes %>% 
   filter(Service_town == "Aberdeen") %>% 
   filter(Council_Area_Name == "Aberdeen City") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(aberdeen_care_homes, "data/report/Aberdeen_care_homes.csv")
@@ -75,8 +75,8 @@ write.csv(aberdeen_care_homes, "data/report/Aberdeen_care_homes.csv")
 perth_care_homes <- care_homes %>% 
   filter(Service_town == "Perth") %>% 
   filter(Council_Area_Name == "Perth & Kinross") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(perth_care_homes, "data/report/perth_care_homes.csv")
@@ -84,8 +84,8 @@ write.csv(perth_care_homes, "data/report/perth_care_homes.csv")
 dunfermline_care_homes <- care_homes %>% 
   filter(Service_town == "Dunfermline") %>% 
   filter(Council_Area_Name == "Fife") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(dunfermline_care_homes, "data/report/dunfermline_care_homes.csv")
@@ -93,8 +93,8 @@ write.csv(dunfermline_care_homes, "data/report/dunfermline_care_homes.csv")
 stirling_care_homes <- care_homes %>% 
   filter(Service_town == "Stirling") %>% 
   filter(Council_Area_Name == "Stirling") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(stirling_care_homes, "data/report/stirling_care_homes.csv")
@@ -104,8 +104,8 @@ write.csv(stirling_care_homes, "data/report/stirling_care_homes.csv")
 inverness_care_homes <- care_homes %>% 
   filter(Service_town == "Inverness") %>% 
   filter(Council_Area_Name == "Highland") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(inverness_care_homes, "data/report/inverness_care_homes.csv")
@@ -113,8 +113,8 @@ write.csv(inverness_care_homes, "data/report/inverness_care_homes.csv")
 elgin_care_homes <- care_homes %>% 
   filter(Service_town == "Elgin") %>% 
   filter(Council_Area_Name == "Moray") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(elgin_care_homes, "data/report/elgin_care_homes.csv")
@@ -128,16 +128,16 @@ nursery <- df %>%
   select(-c(CareService, GradeSpread, ServiceStatus)) %>% 
   rowwise() %>%
   mutate(`Average` = mean(c_across(starts_with("KQ")), na.rm = TRUE)) %>% 
-  mutate(`Complaints_upheld_since_22_23` = sum(c_across(starts_with("Complaints_upheld_")), na.rm = TRUE)) %>% 
-  mutate(`Enforcements_upheld_since_22_23` = sum(c_across(starts_with("Enforcements_issued_")), na.rm = TRUE))
+  mutate(`Complaints_upheld_since_23_24` = sum(c_across(starts_with("Complaints_upheld_")), na.rm = TRUE)) %>% 
+  mutate(`Enforcements_upheld_since_23_24` = sum(c_across(starts_with("Enforcements_issued_")), na.rm = TRUE))
 
 
 
 dundee_nursery <- nursery %>% 
   filter(Service_town == "Dundee") %>% 
   filter(Council_Area_Name == "Dundee City") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(dundee_nursery, "data/report/dundee_nursery.csv")
@@ -145,8 +145,8 @@ write.csv(dundee_nursery, "data/report/dundee_nursery.csv")
 aberdeen_nursery <- nursery %>% 
   filter(Service_town == "Aberdeen") %>% 
   filter(Council_Area_Name == "Aberdeen City") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(aberdeen_nursery, "data/report/Aberdeen_nursery.csv")
@@ -154,8 +154,8 @@ write.csv(aberdeen_nursery, "data/report/Aberdeen_nursery.csv")
 perth_nursery <- nursery %>% 
   filter(Service_town == "Perth") %>% 
   filter(Council_Area_Name == "Perth & Kinross") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(perth_nursery, "data/report/perth_nursery.csv")
@@ -163,8 +163,8 @@ write.csv(perth_nursery, "data/report/perth_nursery.csv")
 dunfermline_nursery <- nursery %>% 
   filter(Service_town == "Dunfermline") %>% 
   filter(Council_Area_Name == "Fife") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(dunfermline_nursery, "data/report/dunfermline_nursery.csv")
@@ -172,8 +172,8 @@ write.csv(dunfermline_nursery, "data/report/dunfermline_nursery.csv")
 stirling_nursery <- nursery %>% 
   filter(Service_town == "Stirling") %>% 
   filter(Council_Area_Name == "Stirling") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>%  
   arrange(desc(Average)) 
 
 write.csv(stirling_nursery, "data/report/stirling_nursery.csv")
@@ -183,8 +183,8 @@ write.csv(stirling_nursery, "data/report/stirling_nursery.csv")
 inverness_nursery <- nursery %>% 
   filter(Service_town == "Inverness") %>% 
   filter(Council_Area_Name == "Highland") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(inverness_nursery, "data/report/inverness_nursery.csv")
@@ -192,8 +192,8 @@ write.csv(inverness_nursery, "data/report/inverness_nursery.csv")
 elgin_nursery <- nursery %>% 
   filter(Service_town == "Elgin") %>% 
   filter(Council_Area_Name == "Moray") %>% 
-  select(-c(Complaints_upheld_2223, Complaints_upheld_2324, Enforcements_issued_2223, Enforcements_issued_2324)) %>% 
-  arrange(Complaints_upheld_since_22_23) %>% 
+  select(-c(Complaints_upheld_2425, Complaints_upheld_2324, Enforcements_issued_2425, Enforcements_issued_2324)) %>% 
+  arrange(Complaints_upheld_since_23_24) %>% 
   arrange(desc(Average)) 
 
 write.csv(elgin_nursery, "data/report/elgin_nursery.csv")
@@ -207,8 +207,8 @@ averages <- df %>%
   select(-c( GradeSpread, ServiceStatus)) %>% 
   rowwise() %>%
   mutate(`Average` = mean(c_across(starts_with("KQ")), na.rm = TRUE)) %>% 
-  mutate(`Complaints_upheld_since_22_23` = sum(c_across(starts_with("Complaints_upheld_")), na.rm = TRUE)) %>% 
-  mutate(`Enforcements_upheld_since_22_23` = sum(c_across(starts_with("Enforcements_issued_")), na.rm = TRUE))
+  mutate(`Complaints_upheld_since_23_24` = sum(c_across(starts_with("Complaints_upheld_")), na.rm = TRUE)) %>% 
+  mutate(`Enforcements_upheld_since_23_24` = sum(c_across(starts_with("Enforcements_issued_")), na.rm = TRUE))
 
 averages <- averages %>% 
   group_by(Council_Area_Name, CareService) %>% 

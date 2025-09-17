facc6e3336561d78ec661effaa31d9a6dec4d961
DC Thomson Data Team <datateam@dctmedia.co.uk>
2025-09-17 15:16:00 +0100

    Update reporter-script.R
    August data update.

--- script/reporter-script.R
+++ script/reporter-script.R
@@ -5,7 +5,7 @@ library(lubridate)
 
 # Add link directly from CI datastore
 
-update <- read_csv("data/datastore/250731DatastoreExternal.csv", locale = locale(encoding = "Windows-1252"))
+update <- read_csv("data/datastore/250831DatastoreExternal.csv", locale = locale(encoding = "Windows-1252"))
 update$ServiceName <- gsub('[^\x20-\x7E]',  '', update$ServiceName) 
 columns_keep <- c("CareService", "Subtype", "ServiceType", "ServiceName",
                   "Service_town", "Service_Postcode", "ServiceProvider", 
